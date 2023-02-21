5360e96c8071b320b9d72ed3c9d3089d8ce2fd26
DevHwangIT <zxc1063@naver.com>
2023-02-21 21:49:45 +0900

    202.02.21 HTML,CSS,JS Study

--- MyHomePage/js/Login.js
+++ MyHomePage/js/Login.js
@@ -5,9 +5,27 @@ HTML에서 JavaScript 연결 테스트
 //call this function after page load
 
 window.onload = function () {
-    var logBtn = document.getElementById('InputArea_Log-In_button');
+    var logBtn = document.getElementById("InputArea_Log-In_button");
     logBtn.addEventListener('click', function () {
-        alert('Try Log-In');
+        const id = document
+            .getElementById("id_Input_Field")
+            .value;
+        const pw = document
+            .getElementById("pw_Input_Field")
+            .value;
+
+        if (id == 'Manager' && pw == '') {
+            alert('성공적으로 아이디나 비밀번호를 입력하셨습니다.!!');
+            //새로운 페이지가 아닌 현재 페이지를 변경시키기
+            location.href = 'NoticeBoard.html';
+            
+            //새로운 페이지 열기 방법
+//             var mainUrl = "NoticeBoard.html";
+//             var popOption = "top=10, left=10, width=500, height=600, status=no, menubar = no,resizable = no";
+//  window.open(mainUrl, popOption);
+//         } else {
+//             alert('잘못된 아이디나 비밀번호를 입력하셨습니다.!!');
+        }
     })
     var cancelBtn = document.getElementById('InputArea_Cancel_button');
     cancelBtn.addEventListener('click', function () {
